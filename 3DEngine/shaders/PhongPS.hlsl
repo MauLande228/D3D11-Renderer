23291b8b394ef44a63f3d79d273c1abb63f46db1
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2022-08-01 18:21:41 -0500

    Material color bug fixed
    Wrong texture format in texture2d description

--- 3DEngine/shaders/PhongPS.hlsl
+++ 3DEngine/shaders/PhongPS.hlsl
@@ -16,9 +16,6 @@ cbuffer Object
     float padding[2];
 };
 
-Texture2D tex;
-SamplerState splr;
-
 struct VsOut
 {
     float3 worldPos : POSITION;
@@ -27,26 +24,29 @@ struct VsOut
     float4 pos : SV_Position;
 };
 
-float4 main(VsOut vsin) : SV_TARGET
+Texture2D tex;
+SamplerState splr;
+
+float4 main(VsOut input) : SV_TARGET
 {
-	// Fragment to light vector data
-    const float3 vToL = lightPos - vsin.worldPos;
+	//Fragment to Light vector data
+    const float3 vToL = lightPos - input.worldPos;
     const float distToL = length(vToL);
     const float3 dirToL = vToL / distToL;
     
-    // Diffuse attenuation
-    const float att = 1.0f / (AttConst + AttLin * distToL * AttQuad * (distToL * distToL));
+    //Diffuse attenuation
+    const float att = 1.0f / (AttConst + AttLin * distToL + AttQuad * (distToL * distToL));
     
-    // Diffuse intensity
-    const float3 diffuse = DiffuseColor * DiffuseIntensity * att * max(0.0f, dot(dirToL, vsin.normal));
+    //Diffise intensity
+    const float3 diffuse = DiffuseColor * DiffuseIntensity * att * max(0.0f, dot(dirToL, input.normal));
     
-    // Reflected light vector
-    const float3 w = vsin.normal * dot(vToL, vsin.normal);
+    //Reflected light vector
+    const float3 w = input.normal * dot(vToL, input.normal);
     const float3 r = w * 2.0f - vToL;
     
-    // Calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
-    const float3 specular = att * (DiffuseColor * DiffuseIntensity) * SpecularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(vsin.worldPos))), SpecularPower);
+    // calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
+    const float3 specular = att * (DiffuseColor * DiffuseIntensity) * SpecularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(input.worldPos))), SpecularPower);
     
-    // Final color
-    return float4(saturate((diffuse + Ambient) * tex.Sample(splr, vsin.tc).rgb + specular), 1.0f);
+    //Final color
+    return float4(saturate((diffuse + Ambient) * tex.Sample(splr, input.tc).rgb + specular), 1.0f);
 }
